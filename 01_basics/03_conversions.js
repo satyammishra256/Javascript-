091a726285079022645b71fc304b3ece86440f67
satyam mishra <paradisegangsta171@gmail.com>
2026-06-21 16:20:30 +0530

    Create 03_conversions.js
    conversions of datatypes

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,28 @@
+let name="Satyam"
+let nameinNumber=Number(name)
+console.log(nameinNumber)
+console.log(typeof nameinNumber)
+//CONVERSIONS INTO NUMBER
+//"33" => 33
+//"33abc" => NaN
+//true=> 1
+//false => 0
+
+//Conversions into boolean
+let isloggedin = "Satyam"
+let booleanisloggedIn=Boolean(isloggedin)
+console.log(booleanisloggedIn)
+
+let numValue=7
+let boolValue=Boolean(numValue)
+console.log(boolValue)
+
+
+// Conversion into string
+let someNumber=46
+let someString=String(someNumber)
+console.log(typeof someString)
+console.log(someString)
+let score=null
+let scoreNum=Number(score)
+console.log(scoreNum)
