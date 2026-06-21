ab2e785cb61077f0f07d9a8aaab8671762c4bdd3
satyam mishra <paradisegangsta171@gmail.com>
2026-06-21 12:07:39 +0530

    variables
    Updated variable values and removed unused code. Added comments about variable declarations.

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,14 @@
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+const accountId=12345
+let accountEmail="satyam@112233"
+var accountPassword="98765"
+accountCity="prayagraj"
+let accountState
 
-// accountId = 2 // not allowed
-
-
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
+//accountId=2 this is not allowed as const cant be redeclared and re-assign
+accountEmail="satyam@164"
+accountPassword="02052006"
+accountCity="edinburgh"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
 /*
 Prefer not to use var
@@ -19,4 +16,3 @@ because of issue in block scope and functional scope
 */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
