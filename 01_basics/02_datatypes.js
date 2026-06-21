b646f1ca616ee74cf7d275a7f4c33bdca264c33e
satyam mishra <paradisegangsta171@gmail.com>
2026-06-21 05:57:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // this will treat all js code as newer version of javascript
+// alert(3+3) // we are using node js not browser 
+let name="satyam"
+let age=20
+let isloggedin=true
+let state;
+// DATATYPES IN JAVASCRIPT
+//number
+//string
+//boolean
+//null
+//undefined
+//symbol
+//object
+
+console.log(typeof state)
+console.log(typeof isloggedin)
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
